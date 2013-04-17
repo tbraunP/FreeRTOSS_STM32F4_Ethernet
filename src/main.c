0862060bec7c06902582ff31915f3920ddce3893
Tobias <tbraun@inf>
2013-04-18 00:21:35 +0200

    Anbindung LAN

--- src/main.c
+++ src/main.c
@@ -1,113 +1,184 @@
-#include "stm32f4xx.h"
-#include "uart.h"
-#include "ansi.h"
-#include "ustime.h"
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @author  MCD Application Team
+  * @version V1.0.0
+  * @date    31-October-2011
+  * @brief   Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * THE PRESENT FIRMWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
+  * WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE
+  * TIME. AS A RESULT, STMICROELECTRONICS SHALL NOT BE HELD LIABLE FOR ANY
+  * DIRECT, INDIRECT OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING
+  * FROM THE CONTENT OF SUCH FIRMWARE AND/OR THE USE MADE BY CUSTOMERS OF THE
+  * CODING INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
+  *
+  * <h2><center>&copy; COPYRIGHT 2011 STMicroelectronics</center></h2>
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4x7_eth.h"
+#include "netconf.h"
+#include "main.h"
 #include "FreeRTOS.h"
 #include "task.h"
-#include <math.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include "lwip/tcpip.h"
+#include "serial_debug.h"
 
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
 
-#define LED_RED		(GPIO_Pin_14)
-#define LED_BLUE	(GPIO_Pin_15)
-#define LED_ORANGE	(GPIO_Pin_13)
-#define LED_GREEN	(GPIO_Pin_12)
+/*--------------- LCD Messages ---------------*/
+#define MESSAGE1   "     STM32F4x7      "
+#define MESSAGE2   "  STM32F-4 Series   "
+#define MESSAGE3   " UDP/TCP EchoServer "
+#define MESSAGE4   "                    "
 
-#define LED_On(led)		GPIOD ->BSRRL |= led
-#define LED_Off(led)	GPIOD ->BSRRH |= led
+/*--------------- Tasks Priority -------------*/
+#define DHCP_TASK_PRIO   ( tskIDLE_PRIORITY + 2 )      
+#define LED_TASK_PRIO    ( tskIDLE_PRIORITY + 1 )
 
-void LED_init(){
-	/* GPIOD Periph clock enable */
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+extern struct netif xnetif;
+__IO uint32_t test;
+ 
+/* Private function prototypes -----------------------------------------------*/
+void LCD_LED_Init(void);
+extern void tcpecho_init(void);
+extern void udpecho_init(void);
 
-	/* Configure PD12, 13, 14 and PD15 in output pushpull mode */
-	GPIO_InitTypeDef GPIO_InitStructure;
+/* Private functions ---------------------------------------------------------*/
 
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14
-			| GPIO_Pin_15;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
-	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
-	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
-	GPIO_Init(GPIOD, &GPIO_InitStructure);
-}
-
-
-void set_leds(int leds)
+/**
+  * @brief  Toggle Led4 task
+  * @param  pvParameters not used
+  * @retval None
+  */
+void ToggleLed4(void * pvParameters)
 {
-    GPIOD->ODR = (GPIOD->ODR & ~0xF000) | ((leds << 12) & 0xF000);
-}
-
-struct task_param {
-    char *name;
-    int   interval;
-};
-
-static void fpu_task(void *pvParameters)
-{
-    LED_On(LED_BLUE);
-    // Force x to stay in a FPU reg.
-    //
-    register float x = 0;
-    struct task_param *p = pvParameters;
-
-    for(;;) {
-        printf("%s: x = %f\n", p->name, x);
-        x += 1;
-        vTaskDelay(p->interval);
-        LED_On(LED_BLUE);
-        vTaskDelay(p->interval);
-        LED_Off(LED_BLUE);
+  while(1)
+  {   
+    test = xnetif.ip_addr.addr;
+    /*check if IP address assigned*/
+    if (test !=0)
+    {
+      for( ;; )
+      {
+        /* toggle LED4 each 250ms */
+        STM_EVAL_LEDToggle(LED4);
+        vTaskDelay(250);
+      }
     }
-   
+  }
 }
 
-static void init_task(void *pvParameters)
-{
- LED_On(LED_ORANGE);
-    RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN;
-    GPIO_Init(GPIOD, &(GPIO_InitTypeDef) {
-        .GPIO_Pin   = 0xF000,
-        .GPIO_Mode  = GPIO_Mode_OUT,
-        .GPIO_OType = GPIO_OType_PP,
-        .GPIO_PuPd  = GPIO_PuPd_NOPULL,
-        .GPIO_Speed = GPIO_Speed_50MHz,
-    });
-
-    uart_init(115200);
-    printf(ANSI_FG_LTRED "STM32F407" ANSI_NORMAL " FreeRTOS Test\n" );
-
-    vTaskDelay(100);
-
-    for (int i=0; i<5; i++) {
-        printf("Starting FPU task %d..\n", i);
-
-        struct task_param *p;
-
-        p = malloc(sizeof(struct task_param));
-        p->name     = malloc(16);
-        p->interval = (i+1) * 500;
-        sprintf(p->name, "FPU_%d", i);
-        LED_On(LED_GREEN);
-
-        xTaskCreate(fpu_task, (int8_t*)p->name, 1024, p, tskIDLE_PRIORITY, NULL);
-    }
-
-    for(;;);
-}
-
-
+/**
+  * @brief  Main program.
+  * @param  None
+  * @retval None
+  */
 int main(void)
 {
-    // FreeRTOS assumes 4 preemption- and 0 subpriority-bits
-    //
-    NVIC_PriorityGroupConfig(NVIC_PriorityGroup_4);
+  /*!< At this stage the microcontroller clock setting is already configured to 
+       168 MHz, this is done through SystemInit() function which is called from
+       startup file (startup_stm32f4xx.s) before to branch to application main.
+       To reconfigure the default setting of SystemInit() function, refer to
+       system_stm32f4xx.c file
+     */
 
-    LED_init();      
-    LED_On(LED_RED);
-    // Create init task and start the scheduler
-    //
-    xTaskCreate(init_task, (signed char*)"init", 1024, NULL, tskIDLE_PRIORITY, NULL);
-    vTaskStartScheduler();
+#ifdef SERIAL_DEBUG
+  DebugComPort_Init();
+#endif
+  
+  /*Initialize LCD and Leds */ 
+  LCD_LED_Init();
+  
+  /* configure ethernet (GPIOs, clocks, MAC, DMA) */ 
+  ETH_BSP_Config();
+    
+  /* Initilaize the LwIP stack */
+  LwIP_Init();
+  
+  /* Initialize tcp echo server */
+  tcpecho_init();
+  
+  /* Initialize udp echo server */
+  udpecho_init();
+
+#ifdef USE_DHCP
+  /* Start DHCPClient */
+  xTaskCreate(LwIP_DHCP_task, "DHCPClient", configMINIMAL_STACK_SIZE * 2, NULL,DHCP_TASK_PRIO, NULL);
+#endif
+    
+  /* Start toogleLed4 task : Toggle LED4  every 250ms */
+  xTaskCreate(ToggleLed4, "LED4", configMINIMAL_STACK_SIZE, NULL, LED_TASK_PRIO, NULL);
+  
+  /* Start scheduler */
+  vTaskStartScheduler();
+
+  /* We should never get here as control is now taken by the scheduler */
+  for( ;; );
 }
+
+/**
+  * @brief  Initializes the STM324xG-EVAL's LCD and LEDs resources.
+  * @param  None
+  * @retval None
+  */
+void LCD_LED_Init(void)
+{
+#ifdef USE_LCD
+  /* Initialize the STM324xG-EVAL's LCD */
+  STM324xG_LCD_Init();
+#endif
+
+  /* Initialize STM324xG-EVAL's LEDs */
+  STM_EVAL_LEDInit(LED1);
+  STM_EVAL_LEDInit(LED2);
+  STM_EVAL_LEDInit(LED3);
+  STM_EVAL_LEDInit(LED4);
+  
+#ifdef USE_LCD
+  /* Clear the LCD */
+  LCD_Clear(Black);
+
+  /* Set the LCD Back Color */
+  LCD_SetBackColor(Black);
+
+  /* Set the LCD Text Color */
+  LCD_SetTextColor(White);
+
+  /* Display message on the LCD*/
+  LCD_DisplayStringLine(Line0, (uint8_t*)MESSAGE1);
+  LCD_DisplayStringLine(Line1, (uint8_t*)MESSAGE2);
+  LCD_DisplayStringLine(Line2, (uint8_t*)MESSAGE3);
+  LCD_DisplayStringLine(Line3, (uint8_t*)MESSAGE4);  
+#endif
+}
+
+#ifdef  USE_FULL_ASSERT
+
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *   where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t* file, uint32_t line)
+{
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+  /* Infinite loop */
+  while (1)
+  {}
+}
+#endif
+
+
+/******************* (C) COPYRIGHT 2011 STMicroelectronics *****END OF FILE****/
